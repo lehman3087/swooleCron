e46878bfdac044f52a40b23cf72146de09b78f78
pengjianwei <pengjw@chexiu.cn>
2016-03-11 10:53:42 +0800

    test

---
+++ jean/swooleCron/Main.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * filename: Main.php
+ * Created by pjianwei.
+ * Date: 2016/3/11 9:23
+ * description: 基于SWOOLE的任务系统的主程序入口
+ */
+
+namespace jean\cron;
+
+
+class Main
+{
+
+    static function run()
+    {
+
+    }
+}
