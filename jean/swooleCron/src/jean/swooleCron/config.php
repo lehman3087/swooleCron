7a6516adc520e30f484a45b46b385576cd94c62e
pengjianwei <pengjw@chexiu.cn>
2016-03-11 11:10:46 +0800

    test

---
+++ jean/swooleCron/src/jean/swooleCron/config.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * filename: config.php
+ * Created by pjianwei.
+ * Date: 2016/3/11 11:08
+ * description:
+ */
---
+++ jean/swooleCron/src/jean/swooleCron/cronConfig.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * filename: cronConfig.php
+ * Created by pjianwei.
+ * Date: 2016/3/11 11:10
+ * description:
+ */
+
+namespace jean\swooleCron;
+
+
+class cronConfig
+{
+
+}
