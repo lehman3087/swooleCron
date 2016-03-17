d99dd108a61161883fb724cf2668e55927b814c3
pengjianwei <pengjw@chexiu.cn>
2016-03-17 17:42:30 +0800

    完成了同步多任务并发执行的功能

--- src/jean/lib/Process.php
+++ src/jean/lib/Process.php
@@ -9,40 +9,100 @@
 namespace jean\lib;
 
 
-class Process extends \swoole_process
+class Process
 {
     protected $workers = [];
     protected $worker_num = 1;
     protected $fun = null;
     protected $data = [];
+    protected $status = [];
 
     public function __construct(array $params)
     {
-        isset($params['worker_num']) and $this->worker_num = intval($params['worker_num']);
+        isset($params['worker_num']) and $this->setWorkNum($params['worker_num']);
         !$this->worker_num and $this->worker_num = 1;
-        if (!isset($params['fun']) && is_callable($params['fun'])) {
-            throw  new  \Exception("请确认回调函数", '10001');
-        }
         $this->fun = $params['fun'];
         isset($params['data']) and $this->data = $params['data'];
     }
 
-    protected function _init()
+    protected function setWorkNum($num)
     {
-
+        $num = intval($num);
+        if ($num > 0 & $num <= 10) {
+            $this->worker_num = $num;
+        }
     }
 
-    public function run()
+    /**
+     * @return $this
+     */
+    public function syncRun()
     {
+        $data = $this->data;
+        $fun = $this->fun;
         for ($i = 0; $i < $this->worker_num; $i++) {
-            $process = new \swoole_process(function () use ($this) {
-                \swoole_process::daemon(true);
-                call_user_func($this->fun, $this->data);
-                exit(0);
+            $process = new \swoole_process(function (\swoole_process $worker) use ($fun, $data, $i) {
+                //\swoole_process::daemon(true);
+                $params = isset($data[$i]) ? $data[$i] : [];
+                try {
+                    if (is_callable($fun)) {
+                        $rs = call_user_func_array($fun, [$params, $i]);
+                    } elseif (isset($fun[$i]) && is_callable($fun[$i])) {
+                        $rs = call_user_func_array($fun[$i], [$params, $i]);
+                    }
+                    if ($rs) {
+                        $worker->write(json_encode($rs));
+                    }
+                } catch (\Exception $e) {
+                    $worker->exit(1);
+                }
+                $worker->exit(1);
             });
             $pid = $process->start();
-            $workers[$pid] = $process;
+            $this->workers[$pid] = $process;
         }
+        return $this;
+    }
+
+    public function result()
+    {
+        $rs = [];
+        $i = 0;
+        foreach ($this->workers as $process) {
+            $str = $process->read();
+            $rs[$i] = $str;
+            if ($str = json_decode($str)) {
+                $rs[$i] = $str;
+            }
+            $i++;
+        }
+        return $rs;
+    }
+
+    public function quit()
+    {
+        for ($i = 0; $i < $this->worker_num; $i++) {
+            $this->status[] = \swoole_process::wait();
+        }
+    }
+
+    public function asyncRun()
+    {
+        //TODO
+        return $this;
+    }
+
+    public function __get($name)
+    {
+        if (isset($this->$name)) {
+            return $this->$name;
+        }
+        return $this;
+    }
+
+    public function __destruct()
+    {
+        $this->quit();
     }
 
 
--- src/jean/swooleCron/Job.php
+++ src/jean/swooleCron/Job.php
@@ -6,23 +6,31 @@
  * description:
  */
 
-namespace jean\swooleCron;
+namespace jean\swoolecron;
 
 use jean\lib\Process;
 
 class Job
 {
     /**
-     * 异步执行任务
+     * 同步多进程并发执行任务.回调函数自行处理异常，返回值必须为真.否则进程会出异常
      * @param array $params 回调执行的参数
      * @example $params = [
-     *  'work_num'=>3,//不给默认为1
-     *  'fun' =>[Server::start] //PHP可执行回调的函数
-     *  'data'=>[];//回调函数的参数
-     * ]
+     *  'work_num'=>3,//不给默认为1,最大值为10
+     *  'fun' =>[Server::start] //PHP可执行回调的函数.可以统一给一个回调函数,也可以指定索引下标分别指定回调函数
+     *  'data'=>[
+     *   0=>[],//索引为0的进程回调的参数
+     *   1=>[],//索引为1的进程回调函数的参数
+     *  ];//回调函数的参数
+     * @return array //按照下标索引分别返回每个进程回调函数的结果,并且每个回调函数返回的结果字符大小限制在8K以内
      */
-    static function run(array $params = [])
+    static function syncRun(array $params = [])
     {
-        return (new Process($params))->run();
+        return (new Process($params))->syncRun()->result();
+    }
+
+    static function asyncRun(array $params)
+    {
+        return (new Process($params))->asyncRun();
     }
 }
